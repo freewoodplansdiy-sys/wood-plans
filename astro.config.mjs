3edb9cc9f610cf2eacd8bca9f66f26027e201886
hasonkobi <freewoodplansdiy@gmail.com>
2026-06-15 09:43:42 +0000

    CHIEF FIX: Pure directory structure and clean build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-export default defineConfig({ integrations: [tailwind()], outDir: 'dist' });
+export default defineConfig({
+  integrations: [tailwind()],
+  outDir: 'dist'
+});
